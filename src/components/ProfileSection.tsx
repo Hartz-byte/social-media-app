294b9eabd8ac857578ffd56f6f74b78f25065b3f
Harsh Gupta <harshmail281199@gmail.com>
2024-11-17 04:32:03 +0530

    ProfileSection component completed with dummy data

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -1,7 +1,52 @@
+import CoverPic from "../assets/images/CoverPic.jpg";
+import ProfilePic from "../assets/images/ProfilePic.jpg";
+
 const ProfileSection = () => {
   return (
-    <div className="w-[20%] flex items-center justify-center bg-[#2a2a2a] rounded-xl">
-      <p>Profile Section</p>
+    <div className="w-[20%] flex flex-col items-center bg-[#2a2a2a] rounded-xl">
+      {/* Cover and Profile Picture */}
+      <div className="relative w-full h-32 rounded-t-xl overflow-hidden mb-8">
+        {/* Cover Photo */}
+        <img
+          src={CoverPic}
+          alt="Cover Photo"
+          className="w-full h-full object-cover rounded-t-xl"
+        />
+
+        {/* Profile Picture */}
+        <img
+          src={ProfilePic}
+          alt="Profile"
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-24 h-24 rounded-full object-cover border-2 border-white"
+        />
+      </div>
+
+      {/* User Info */}
+      <div className="text-center text-white">
+        <p className="text-xl font-semibold">Harsh Gupta</p>
+        <p className="text-md text-gray-400">@hrsh_line_up</p>
+        <p className="text-sm text-gray-500 mt-5">Believe in yourself</p>
+      </div>
+
+      {/* Following and Followers */}
+      <div className="flex mt-5 text-white">
+        {/* Followers */}
+        <div className="flex flex-col items-center mx-4">
+          <p className="text-lg font-semibold">Followers</p>
+          <p className="text-xl">150</p>
+        </div>
+
+        {/* Following */}
+        <div className="flex flex-col items-center mx-4">
+          <p className="text-lg font-semibold">Following</p>
+          <p className="text-xl">120</p>
+        </div>
+      </div>
+
+      {/* My Profile Button */}
+      <button className="mt-10 w-[90%] py-2 bg-[#2f2e2e] text-white rounded-lg hover:bg-[#242424] transition-colors">
+        My Profile
+      </button>
     </div>
   );
 };
