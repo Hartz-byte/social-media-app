2f54c4b7b70181f805d7e5bc660a7e727939dbdc
Harsh Gupta <harshmail281199@gmail.com>
2024-11-17 00:13:51 +0530

    SplashScreen page completed

--- src/pages/SplashScreen.tsx
+++ src/pages/SplashScreen.tsx
@@ -1,5 +1,58 @@
-const SplashScreen = () => {
-  return <div>SplashScreen</div>;
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Logo from "../assets/logo/Buzzz.jpg";
+
+const SplashScreen: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [showMainText, setShowMainText] = useState(false);
+  const [showSubText, setShowSubText] = useState(false);
+
+  useEffect(() => {
+    // main text after the logo animation
+    const mainTextTimeout = setTimeout(() => setShowMainText(true), 1000);
+    // subtext after the main text animation
+    const subTextTimeout = setTimeout(() => setShowSubText(true), 2000);
+
+    // navigate to the login page after animations
+    const navigateTimeout = setTimeout(() => {
+      navigate("/login");
+    }, 5000);
+
+    return () => {
+      clearTimeout(mainTextTimeout);
+      clearTimeout(subTextTimeout);
+      clearTimeout(navigateTimeout);
+    };
+  }, [navigate]);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[#1a1a1a]">
+      <div className="flex flex-col items-center">
+        {/* logo animation */}
+        <img
+          src={Logo}
+          alt="Buzzz Logo"
+          className={`animate-bounceIn w-24 h-24 mb-4`}
+        />
+
+        {/* main text animation */}
+        {showMainText && (
+          <h1 className="font-gravitas text-3xl font-bold text-white animate-fadeIn">
+            BUZZZ
+          </h1>
+        )}
+
+        {/* subtext animation */}
+        {showSubText && (
+          <h2 className="mt-2 text-[#B39757] text-lg font-medium overflow-hidden whitespace-nowrap animate-typing">
+            WHERE CONNECTIONS HUMMM!!
+          </h2>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SplashScreen;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        bounceIn: {
+          "0%": { transform: "scale(0)", opacity: 0 },
+          "50%": { transform: "scale(1.1)", opacity: 0.8 },
+          "100%": { transform: "scale(1)", opacity: 1 },
+        },
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        typing: {
+          "0%": { width: "0%" },
+          "100%": { width: "100%" },
+        },
+      },
+      animation: {
+        bounceIn: "bounceIn 1s ease-out",
+        fadeIn: "fadeIn 1s ease-in-out",
+        typing: "typing 2s steps(20, end) forwards",
+      },
+    },
+    fontFamily: {
+      gravitas: ["Gravitas One", "sans-serif"],
+    },
   },
   plugins: [],
 };
