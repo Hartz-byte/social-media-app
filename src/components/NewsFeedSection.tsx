2e95b53e12db01beacce76731f3c72a911c5fe1e
Harsh Gupta <harshmail281199@gmail.com>
2024-11-17 13:46:13 +0530

    NewsFeedSection profile pic and input field ui done

--- src/components/NewsFeedSection.tsx
+++ src/components/NewsFeedSection.tsx
@@ -1,7 +1,56 @@
+import ProfilePic from "../assets/images/ProfilePic.jpg";
+
 const NewsFeedSection = () => {
   return (
-    <div className="w-[60%] flex items-center justify-center">
-      <p>News Feed Section</p>
+    <div className="w-[60%] flex flex-col items-center">
+      {/* Container */}
+      <div className="w-full bg-[#2a2a2a] p-4 rounded-xl flex flex-col">
+        {/* Profile Pic and Input Field */}
+        <div className="flex items-center mb-4">
+          {/* Profile Pic */}
+          <img
+            src={ProfilePic}
+            alt="Profile"
+            className="w-12 h-12 object-cover rounded-full mr-4"
+          />
+
+          {/* Input Field */}
+          <input
+            type="text"
+            placeholder="Tell your friends about your thoughts..."
+            className="flex-1 h-12 bg-[#242424] text-white p-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-[#B39757]"
+          />
+        </div>
+
+        {/* Icons Section */}
+        <div className="flex justify-between ml-16">
+          {/* Gallery */}
+          <div className="flex items-center bg-[#242424] p-2 pl-4 pr-4 rounded-xl cursor-pointer hover:bg-[#1e1e1e]">
+            <span className="material-icons text-[#20D997] mr-2">photo</span>
+            <p className="text-white">Gallery</p>
+          </div>
+
+          {/* Video */}
+          <div className="flex items-center bg-[#242424] p-2 pl-4 pr-4 rounded-xl cursor-pointer hover:bg-[#1e1e1e]">
+            <span className="material-icons text-[#4F94FC] mr-2">videocam</span>
+            <p className="text-white">Video</p>
+          </div>
+
+          {/* Poll */}
+          <div className="flex items-center bg-[#242424] p-2 pl-4 pr-4 rounded-xl cursor-pointer hover:bg-[#1e1e1e]">
+            <span className="material-icons text-[#DF7272] mr-2">poll</span>
+            <p className="text-white">Poll</p>
+          </div>
+
+          {/* Schedule */}
+          <div className="flex items-center bg-[#242424] p-2 pl-4 pr-4 rounded-xl cursor-pointer hover:bg-[#1e1e1e]">
+            <span className="material-icons text-[#DFB761] mr-2">
+              calendar_today
+            </span>
+            <p className="text-white">Schedule</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
