94f26f74c41d27a41a0cfbbc59be9ac956424203
Harsh Gupta <harshmail281199@gmail.com>
2024-11-17 14:02:54 +0530

    PrivateRoute updated with the onAuthStateChanged function, and firebaseConfig updated with the setPersistence

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,5 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCZArXcxHb15qQaMgUq843OdUOMZzRXG0Y",
@@ -15,3 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+setPersistence(auth, browserLocalPersistence);
--- src/navigation/PrivateRoute.tsx
+++ src/navigation/PrivateRoute.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const PrivateRoute: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
   const auth = getAuth();
-  const user = auth.currentUser;
 
-  return user ? <Outlet /> : <Navigate to="/login" />;
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      // Set true if user exists otherwise false
+      setIsAuthenticated(!!user);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  // While Firebase is determining the authentication state, don't render anything.
+  if (isAuthenticated === null) {
+    return null;
+  }
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
